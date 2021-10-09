ba2aa0a8ff19d645ee72271b997238969dae4c7e
mac <mac@macdemacbook-pro.local>
2021-10-09 19:23:21 +0800

    Initial Commit

---
+++ OpenGLTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLTest
+//
+//  Created by mac on 2021/10/9.
+//  Copyright © 2021 mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
